dc0d8e053e8ce192dc024514d237b379419674bd
LeandroMenoci <menoci.leandro@gmail.com>
2022-05-03 13:03:21 -0300

    Primeira parte do feedback

--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -1,6 +1,7 @@
 import { Popover } from '@headlessui/react'
 import { ChatTeardropDots } from 'phosphor-react'
 import { useState } from 'react'
+import { WidgetForm } from './WidgetForm'
 
 export function Widget() {
   const [isWidgetOpen, setIsWidgetOpen] = useState(false)
@@ -9,8 +10,10 @@ export function Widget() {
     setIsWidgetOpen(!isWidgetOpen)
   }
   return (
-    <Popover className='absolute bottom-5 right-5'>
-      <Popover.Panel>hello world</Popover.Panel>
+    <Popover className='absolute bottom-4 right-4 md:bottom-8 md: right-8  flex flex-col items-end'>
+      <Popover.Panel>
+        <WidgetForm />
+      </Popover.Panel>
       <Popover.Button className='bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group'>
         <ChatTeardropDots className='w-6 h-6' />
 
